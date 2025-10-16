82c0ba852fd1f1e452df829e43f42af1b468bfa0
KroegerLeif <Leif.kr@outlook.com>
2025-10-16 12:09:46 +0200

    Creation of CreateTaskDTO

---
+++ backend/src/main/java/org/example/backend/controller/dto/CreateTaskDTO.java
@@ -0,0 +1,15 @@
+package org.example.backend.controller.dto;
+
+import org.example.backend.domain.item.Item;
+import org.example.backend.domain.task.Priority;
+import org.example.backend.domain.task.Status;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record CreateTaskDTO(String name,
+                            List<Item> items,
+                            Priority priority,
+                            Status status,
+                            LocalDate dueDate) {
+}
