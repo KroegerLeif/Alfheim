c98208782109a4105807dd0a9f51fd10f27cf70c
KroegerLeif <Leif.kr@outlook.com>
2025-10-16 12:04:18 +0200

    Creation of TaskTableReturnDTO

---
+++ backend/src/main/java/org/example/backend/controller/dto/TaskTableReturnDTO.java
@@ -0,0 +1,15 @@
+package org.example.backend.controller.dto;
+
+import org.example.backend.domain.task.Priority;
+import org.example.backend.domain.task.Status;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record TaskTableReturnDTO(String name,
+                                 List<String> items,
+                                 List<String> assignedTo,
+                                 Priority priority,
+                                 Status status,
+                                 LocalDate dueDate) {
+}
