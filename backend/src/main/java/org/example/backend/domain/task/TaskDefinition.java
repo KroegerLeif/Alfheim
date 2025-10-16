2c743de25969e5e8351a0fb10f9834511b439fd3
KroegerLeif <Leif.kr@outlook.com>
2025-10-16 10:48:09 +0200

    Added User conection and Item conection to TaskDefinition

--- backend/src/main/java/org/example/backend/domain/task/TaskDefinition.java
+++ backend/src/main/java/org/example/backend/domain/task/TaskDefinition.java
@@ -1,9 +1,15 @@
 package org.example.backend.domain.task;
 
+import org.example.backend.domain.item.Item;
+import org.example.backend.domain.user.User;
+
 import java.math.BigDecimal;
+import java.util.List;
 
 public record TaskDefinition(String id,
                              String name,
+                             List<User> responsible,
+                             List<Item> connectedItems,
                              BigDecimal price,
                              Priority priority,
                              int repetition) {
