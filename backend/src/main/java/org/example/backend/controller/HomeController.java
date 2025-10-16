1115a3f52145bd6ca388d2f44cde5f84fdfafaec
KroegerLeif <Leif.kr@outlook.com>
2025-10-16 16:17:06 +0200

    URI api/home insted of /home

--- backend/src/main/java/org/example/backend/controller/HomeController.java
+++ backend/src/main/java/org/example/backend/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/api/home")
 public class HomeController {
 
     private final HomeService homeService;
