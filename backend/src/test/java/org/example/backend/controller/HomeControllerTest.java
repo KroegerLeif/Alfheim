0d61b4cf6793bb04908f12a9d3646f4e44894043
KroegerLeif <Leif.kr@outlook.com>
2025-10-17 14:03:54 +0200

    Added Test Controller

--- backend/src/test/java/org/example/backend/controller/HomeControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/HomeControllerTest.java
@@ -1,18 +1,19 @@
 package org.example.backend.controller;
 
-
+import org.example.backend.domain.home.Address;
+import org.example.backend.domain.home.Home;
+import org.example.backend.repro.HomeRepro;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,49 +23,46 @@ class HomeControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private MockRestServiceServer mockRestServiceServer;
+    private HomeRepro homeRepro;
+
+    @AfterEach
+    void tearDown() {
+        homeRepro.deleteAll();
+    }
 
     @Test
     void getAllHomes_shouldReturnEmptyList_whenNoHomesExist() throws Exception {
-        //GIVEN
-        mockRestServiceServer.expect(requestTo("/api/home"))
-                .andRespond((withSuccess(
-                        """
-                            []    
-                         """, MediaType.APPLICATION_JSON)));
-        //WHEN & THEN
         mockMvc.perform(MockMvcRequestBuilders.get("/api/home"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json(
-                        """
-                            []    
-                         """));
+                .andExpect(MockMvcResultMatchers.content().json("[]"));
     }
 
     @Test
     void getAllHomes_shouldReturnListOfHomes_whenHomesExist() throws Exception {
-        //GIVEN
-        mockRestServiceServer.expect(requestTo("/api/home"))
-                .andRespond((withSuccess(
-                        """
-                            [
-                                {
-                                    "name" : "haus",
-                                    "address" : "test"
-                                }
-                            ]    
-                         """, MediaType.APPLICATION_JSON)));
-        //WHEN & THEN
+        Address address = new Address("1", "street", "postCode", "city", "country");
+        Home home = new Home("1", "home", address, new ArrayList<>(), new ArrayList<>(), new HashMap<>());
+        homeRepro.save(home);
+
         mockMvc.perform(MockMvcRequestBuilders.get("/api/home"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json(
                         """
-                            [
-                                {
-                                    "name" : "haus",
-                                    "address" : "test"
-                                }
-                            ]    
-                         """));
+                                    [
+                                      {
+                                        "name": "home",
+                                        "address": {
+                                          "id": "1",
+                                          "street": "street",
+                                          "postCode": "postCode",
+                                          "city": "city",
+                                          "country": "country"
+                                        },
+                                        "admin": "admin",
+                                        "numberTask": 0,
+                                        "numberItems": 0,
+                                        "members" : []
+                                      }
+                                    ]
+                                    """));
     }
 }
