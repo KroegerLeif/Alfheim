eb22be39a6d454247afdb1ae760a7ddebb66e2c9
KroegerLeif <Leif.kr@outlook.com>
2025-10-17 15:43:10 +0200

    added of Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
 
 function App() {
 
   return (
     <>
+        <Routes>
+            <Route path={"/"} element={null}/>
+        </Routes>
     </>
   )
 }
