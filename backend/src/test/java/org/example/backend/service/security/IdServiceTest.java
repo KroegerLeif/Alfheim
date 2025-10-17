5c3fab2095d3297e6de3fd9288130816406ae2e0
KroegerLeif <Leif.kr@outlook.com>
2025-10-17 14:25:28 +0200

    Added Test for IdServiceTest

--- backend/src/test/java/org/example/backend/service/security/IdServiceTest.java
+++ backend/src/test/java/org/example/backend/service/security/IdServiceTest.java
@@ -1,10 +1,39 @@
 package org.example.backend.service.security;
 
+import org.example.backend.repro.UserRepro;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class IdServiceTest {
 
     @Test
-    void createNewId() {
+    void createNewId_shouldReturnUUID_whenCalled() {
+        //GIVEN
+        UserRepro mockRepro = mock(UserRepro.class);
+        IdService idService = new IdService(mockRepro);
+        when(mockRepro.existsById(anyString())).thenReturn(false);
+        //WHEN
+        var actual = idService.createNewId();
+        //THEN
+        assert actual != null;
+        assert actual.length() == 36;
+
+
+    }
+
+    @Test
+    void createNewId_shouldReturnNull_whenIdExists() {
+        //GIVEN
+        UserRepro mockRepro = mock(UserRepro.class);
+        IdService idService = new IdService(mockRepro);
+        when(mockRepro.existsById(anyString())).thenReturn(true);
+        //WHEN
+        var actual = idService.createNewId();
+        //THEN
+        assert actual == null;
+
     }
 }
