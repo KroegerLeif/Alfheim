0d02c47b20ba741a1a2f84d395b1c86a258fd27a
KroegerLeif <Leif.kr@outlook.com>
2025-10-17 17:28:14 +0200

    Added Sidebar from shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
+import Navigation from "@/pages/Navigation.tsx";
 
 function App() {
 
   return (
     <>
+        <Navigation/>
         <Routes>
             <Route path={"/"} element={null}/>
         </Routes>
