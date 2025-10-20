f74f69075329c8d82bbb3869e32ee554902866c0
Leif <Leif.kr@outlook.com>
2025-10-20 10:32:31 +0200

    Deletion of sonar frontend

--- frontend/src/pages/Home_page.tsx
+++ frontend/src/pages/Home_page.tsx
@@ -3,6 +3,7 @@ import HomeTable from "@/app/home-table/page.tsx";
 function Home_page(){
     return(
         <div className="homePage">
+            <h1>Home Overview</h1>
             <HomeTable/>
         </div>
     )
