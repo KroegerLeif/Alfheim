aaca2a0d455863b67ea637c512a5ee31f5ca54b3
Leif <Leif.kr@outlook.com>
2025-10-20 20:11:14 +0200

    All task are now shown in a table

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,16 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
 import type {HomeTableReturnDTO} from "@/dto/HomeTableReturnDTO.ts";
 
 function HomePage(){
@@ -21,11 +31,30 @@ function HomePage(){
     return(
         <div className="homePage">
             <h1>Home Overview</h1>
-            {
-                homeTableData.map((home_data) => (
-                    <h1 key={home_data.id}>{home_data.name}</h1>
-                ))
-            }
+            <Table>
+                <TableCaption>A list of all Homes</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Name</TableHead>
+                        <TableHead>Number Task</TableHead>
+                        <TableHead>Number Items</TableHead>
+                        <TableHead>Members</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                        {
+                            homeTableData.map((home_data) => (
+                                <TableRow>
+                                    <TableCell className="font-medium" key={home_data.id}>{home_data.name}</TableCell>
+                                    <TableCell>{home_data.numberTask}</TableCell>
+                                    <TableCell>{home_data.numberItems}</TableCell>
+                                    <TableCell>{home_data.members}</TableCell>
+                                </TableRow>
+                            ))
+                        }
+                </TableBody>
+            </Table>
+
         </div>
     )
 }
