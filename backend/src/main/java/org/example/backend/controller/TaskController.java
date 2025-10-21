06609f3fce2d4738abad12f756db291ae71e0334
Leif <Leif.kr@outlook.com>
2025-10-21 12:48:49 +0200

    Creation of constant in Controller clas

--- backend/src/main/java/org/example/backend/controller/TaskController.java
+++ backend/src/main/java/org/example/backend/controller/TaskController.java
@@ -2,6 +2,8 @@ package org.example.backend.controller;
 
 import org.example.backend.controller.dto.CreateTaskDTO;
 import org.example.backend.controller.dto.TaskTableReturnDTO;
+import org.example.backend.service.TaskService;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/task")
 public class TaskController {
 
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @PostMapping("/create")
     public ResponseEntity<TaskTableReturnDTO> createTask(@RequestBody CreateTaskDTO createTaskDTO) {
-        TaskTableReturnDTO createdTask = taskService.createNewHome(createTaskDTO);
+        TaskTableReturnDTO createdTask = taskService.createNewTask(createTaskDTO);
         return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
     }
 
