36a1f27189dca97ad0ccc5f6686e3a48305a1f58
Leif <Leif.kr@outlook.com>
2025-10-21 19:39:33 +0200

    Test for TaskSeries Repro and I can now look in the repro for the ID and get a TaksService in return

--- backend/src/main/java/org/example/backend/repro/TaskSeriesRepro.java
+++ backend/src/main/java/org/example/backend/repro/TaskSeriesRepro.java
@@ -4,4 +4,6 @@ import org.example.backend.domain.task.TaskSeries;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TaskSeriesRepro extends MongoRepository<TaskSeries, String> {
+
+    TaskSeries getTaskSeriesById(String number);
 }
