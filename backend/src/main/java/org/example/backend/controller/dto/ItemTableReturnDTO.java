c58138dce9e82581375cb5ffc0bb0ff5c9c35d35
Leif <Leif.kr@outlook.com>
2025-10-22 12:26:18 +0200

    Now also returns an ID

--- backend/src/main/java/org/example/backend/controller/dto/ItemTableReturnDTO.java
+++ backend/src/main/java/org/example/backend/controller/dto/ItemTableReturnDTO.java
@@ -2,7 +2,8 @@ package org.example.backend.controller.dto;
 
 import org.example.backend.domain.item.EnergyLabel;
 
-public record ItemTableReturnDTO(String name,
+public record ItemTableReturnDTO(String id,
+                                 String name,
                                  EnergyLabel energyLabel,
                                  String category) {
 }
