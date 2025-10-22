be8b7abe5031229c87cf10ff48afe043013427d7
Leif <Leif.kr@outlook.com>
2025-10-22 12:33:17 +0200

    Typo fixed

---
+++ backend/src/main/java/org/example/backend/controller/dto/CreateItemDTO.java
@@ -0,0 +1,8 @@
+package org.example.backend.controller.dto;
+
+import org.example.backend.domain.item.EnergyLabel;
+
+public record CreateItemDTO(String name,
+                            EnergyLabel energyLabel,
+                            String category) {
+}
--- backend/src/main/java/org/example/backend/controller/dto/CreateTableDTO.java
+++
@@ -1,8 +0,0 @@
-package org.example.backend.controller.dto;
-
-import org.example.backend.domain.item.EnergyLabel;
-
-public record CreateTableDTO(String name,
-                             EnergyLabel energyLabel,
-                             String category) {
-}
