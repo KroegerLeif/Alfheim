a25d037339c2aada01264631e1e8cbe6019b5895
Leif <Leif.kr@outlook.com>
2025-10-22 12:29:43 +0200

    Added Spring dependency

--- backend/src/main/java/org/example/backend/repro/TaskSeriesRepro.java
+++ backend/src/main/java/org/example/backend/repro/TaskSeriesRepro.java
@@ -2,7 +2,9 @@ package org.example.backend.repro;
 
 import org.example.backend.domain.task.TaskSeries;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskSeriesRepro extends MongoRepository<TaskSeries, String> {
 
     TaskSeries getTaskSeriesById(String number);
