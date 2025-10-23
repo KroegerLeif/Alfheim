b372f875f7cd3667a19a668b7bbc671752cb5df7
Leif <Leif.kr@outlook.com>
2025-10-23 13:01:16 +0200

    Added Test for Item Service

--- backend/src/test/java/org/example/backend/service/ItemServiceTest.java
+++ backend/src/test/java/org/example/backend/service/ItemServiceTest.java
@@ -1,14 +1,20 @@
 package org.example.backend.service;
 
+import org.example.backend.controller.dto.CreateItemDTO;
+import org.example.backend.controller.dto.ItemTableReturnDTO;
+import org.example.backend.domain.item.Category;
+import org.example.backend.domain.item.EnergyLabel;
 import org.example.backend.domain.item.Item;
 import org.example.backend.repro.ItemRepro;
 import org.example.backend.service.mapper.ItemMapper;
+import org.example.backend.service.security.IdService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -20,7 +26,11 @@ class ItemServiceTest {
     @Mock
     private final ItemMapper itemMapper = Mockito.mock(ItemMapper.class);
 
-    private final ItemService itemService = new ItemService(mockRepo, itemMapper);
+    @Mock
+    private final IdService idService = Mockito.mock(IdService.class);
+
+
+    private final ItemService itemService = new ItemService(mockRepo, itemMapper, idService);
 
     @AfterEach
     void tearDown() {
@@ -49,5 +59,31 @@ class ItemServiceTest {
         var actual = itemService.getAll();
         Mockito.verify(mockRepo).findAll();
         assertEquals(2, actual.size());
+    }
+
+    @Test
+    void createNewItem_shouldReturnANewItem_whenCalled(){
+        //GIVEN
+        CreateItemDTO createItemDTO = new CreateItemDTO("Test", EnergyLabel.E, "TestCategory");
+        Category category_beforeId = new Category(null, "TestCategory");
+        Item item_beforeId = new Item(null, "Test", category_beforeId, EnergyLabel.E);
+
+        Category category_afterId = new Category("1", "TestCategory");
+        Item item_afterId = new Item("1", "Test", category_afterId, EnergyLabel.E);
+
+        ItemTableReturnDTO expectedTableReturn = new ItemTableReturnDTO("1", "Test", EnergyLabel.E, "TestCategory");
+
+        when(idService.createNewId()).thenReturn("1");
+        when(itemMapper.mapToItem(createItemDTO)).thenReturn(item_beforeId);
+        when(mockRepo.findCategoryByName("TestCategory")).thenReturn(Optional.empty());
+        when(mockRepo.save(item_afterId)).thenReturn(item_afterId);
+        when(itemMapper.mapToItemTableReturn(item_afterId)).thenReturn(expectedTableReturn);
+
+        //WHEN
+        var actual = itemService.createNewItem(createItemDTO);
+        //THEN
+        assertEquals(expectedTableReturn, actual);
+        Mockito.verify(mockRepo).save(item_afterId);
+
     }
 }
