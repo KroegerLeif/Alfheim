bba9899b2d49e339a7ca802768355c835a33d568
Leif <Leif.kr@outlook.com>
2025-10-23 15:24:32 +0200

    Home Service will now edit a Home if no home with specific ID exist it will thorow a new exeption

--- backend/src/main/java/org/example/backend/service/HomeService.java
+++ backend/src/main/java/org/example/backend/service/HomeService.java
@@ -1,12 +1,14 @@
 package org.example.backend.service;
 
 import org.example.backend.controller.dto.create.CreateHomeDTO;
+import org.example.backend.controller.dto.edit.EditHomeDTO;
 import org.example.backend.controller.dto.response.HomeTableReturnDTO;
 import org.example.backend.domain.home.Home;
 import org.example.backend.repro.HomeRepro;
 
 import org.example.backend.service.mapper.HomeMapper;
 import org.example.backend.service.security.IdService;
+import org.example.backend.service.security.exception.HomeDoesNotExistException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -34,5 +36,24 @@ public class HomeService {
     public HomeTableReturnDTO createNewHome(CreateHomeDTO createHomeDTO) {
         Home home = homeMapper.mapToHome(createHomeDTO).withId(idService.createNewId());
         return homeMapper.mapToHomeTableReturn(homeRepro.save(home));
+    }
+
+    public HomeTableReturnDTO editHome(String id, EditHomeDTO editHomeDTO) throws HomeDoesNotExistException {
+        Home home = homeRepro.findById(id).orElseThrow(() ->(new HomeDoesNotExistException("No Home with this ID found")));
+
+        //Changes Values if Change is provided by User
+        if(editHomeDTO.name() != null){
+            home = home.withName(editHomeDTO.name());
+        }
+
+        if(editHomeDTO.address() != null){
+            home = home.withAddress(editHomeDTO.address());
+        }
+
+        //Saves Home
+        homeRepro.save(home);
+
+        return homeMapper.mapToHomeTableReturn(home);
+
     }
 }
