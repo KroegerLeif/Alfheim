428b201d0b4672b55b6fbe9d79d0e9fd4dfe0e47
Leif <Leif.kr@outlook.com>
2025-10-23 11:25:09 +0200

    You can now add a Unique ID to a Category and the Item Repro will now return a Optional with an Repro

--- backend/src/main/java/org/example/backend/service/ItemService.java
+++ backend/src/main/java/org/example/backend/service/ItemService.java
@@ -1,11 +1,16 @@
 package org.example.backend.service;
 
+import org.example.backend.controller.dto.CreateItemDTO;
 import org.example.backend.controller.dto.ItemTableReturnDTO;
+import org.example.backend.domain.item.Category;
+import org.example.backend.domain.item.Item;
 import org.example.backend.repro.ItemRepro;
 import org.example.backend.service.mapper.ItemMapper;
+import org.example.backend.service.security.IdService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ItemService {
@@ -14,14 +19,46 @@ public class ItemService {
 
     private final ItemMapper itemMapper;
 
-    public ItemService(ItemRepro itemRepro, ItemMapper itemMapper) {
+    private final IdService idService;
+
+
+    public ItemService(ItemRepro itemRepro, ItemMapper itemMapper, IdService idService) {
         this.itemRepro = itemRepro;
         this.itemMapper = itemMapper;
+        this.idService = idService;
     }
 
     public List<ItemTableReturnDTO> getAll(){
         return itemRepro.findAll().stream()
                 .map(itemMapper::mapToItemTableReturn)
                 .toList();
+    }
+
+    public ItemTableReturnDTO createNewItem(CreateItemDTO createItemDTO){
+        Item item = creatUniqueIds(itemMapper.mapToItem(createItemDTO));
+        itemRepro.save(item);
+        return itemMapper.mapToItemTableReturn(item);
+    }
+
+    private Item creatUniqueIds(Item item){
+        //Creation of new IDs
+        String item_id = idService.createNewId();
+
+        Category itemCategory = createUniqueIds(item.category());
+
+        return new Item(
+                item_id,
+                item.name(),
+                itemCategory,
+                item.energyLabel()
+        );
+    }
+
+    private Category createUniqueIds(Category category){
+        //Checks If Category already Exists
+        Optional<Category> categoryOptional = itemRepro.findCategoryByName(category.name());
+        //Create New Item
+        return categoryOptional.orElseGet(() -> category.withId(idService.createNewId()));
+
     }
 }
