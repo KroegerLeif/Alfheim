0773ef1ff1261addb4af05a9b0fb568b74c61daa
Leif <Leif.kr@outlook.com>
2025-10-27 10:40:13 +0100

    Change of typo...varible in the wron file

--- frontend/src/dto/ItemTableReturn.ts
+++ frontend/src/dto/ItemTableReturn.ts
@@ -2,7 +2,6 @@ import type {EnergyLabel} from "@/dto/EnergyLabel.ts";
 
 export type ItemTableReturn ={
     id: string,
-    taskSeriesId: string,
     name: string,
     energyLabel: EnergyLabel,
     category: string
