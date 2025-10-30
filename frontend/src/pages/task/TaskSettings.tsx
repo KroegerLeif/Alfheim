06eea36bf875e8efa1f70c3e5c74d05d7b2e311c
Leif <Leif.kr@outlook.com>
2025-10-30 22:33:10 +0100

    Integration from react hook in Task settings

--- frontend/src/pages/task/TaskSettings.tsx
+++ frontend/src/pages/task/TaskSettings.tsx
@@ -4,11 +4,79 @@ import {
     DialogContent,
     DialogHeader,
     DialogTitle,
-    DialogDescription, DialogClose, DialogFooter,
+    DialogDescription,
+    DialogClose,
+    DialogFooter,
 } from "@/components/ui/dialog.tsx";
+import {
+    Field,
+    FieldDescription,
+    FieldLabel,
+    FieldSet,
+} from "@/components/ui/field";
 import {Button} from "@/components/ui/button.tsx";
+import axios from "axios";
+import type {TaskTableReturn} from "@/dto/response/TaskTableReturn.ts";
+import {Input} from "@/components/ui/input.tsx";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select.tsx";
+import {useEffect, useState} from "react";
+import {Controller, type SubmitHandler, useForm} from "react-hook-form";
+import type {Priority} from "@/dto/Priority.ts";
+import type {Status} from "@/dto/Status.ts";
 
-function TaskSettings(){
+function TaskSettings({taskSeriesId, name, priority, status, dueDate,repetition,homeId, loadTaskData}:
+                      Readonly<TaskTableReturn &
+                      {loadTaskData: () => void} >){
+
+    const [homeList, setHomeList] = useState([
+        {
+            id:"",
+            name:""
+        }
+    ]);
+
+    type FormFields = {
+        name: string;
+        priority: Priority;
+        home: string;
+        status: Status;
+        dueDate: string;
+        repetition: number;
+    };
+
+    const {register, handleSubmit,control} = useForm({
+            defaultValues: {
+                name:name,
+                priority:priority,
+                home:homeId,
+                status:status,
+                dueDate:dueDate,
+                repetition:repetition
+            }
+        }
+    );
+
+    const onSubmit: SubmitHandler<FormFields> = (data) => {
+        console.log(data);
+    }
+
+
+    function deleteTask(id: string){
+        axios.delete("api/task/" + id + "/delete")
+            .then(() => loadTaskData())
+    }
+
+    function getHomeNames(){
+        axios.get("api/home/getNames")
+            .then(response => {
+                setHomeList(response.data);
+            })
+    }
+
+
+    useEffect(() =>{
+        getHomeNames();
+    },[])
 
     return(
         <Dialog>
@@ -17,12 +85,81 @@ function TaskSettings(){
             </DialogTrigger>
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>Edit Home</DialogTitle>
+                    <DialogTitle>Edit Task</DialogTitle>
                     <DialogDescription>
-                        Change Values from Home here.
+                        Change Values from Task here.
                     </DialogDescription>
                 </DialogHeader>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <FieldSet>
+                            <Field>
+                                <FieldLabel htmlFor="name">Task Name</FieldLabel>
+                                <Input {...register("name",
+                                            {required: true})
+                                        } type="text" autoComplete="off" placeholder={name}/>
+                                <FieldDescription>This appears on invoices and emails.</FieldDescription>
+                            </Field>
+                            <Field>
+                                <FieldLabel htmlFor="priority">Priority</FieldLabel>
+                                <Select {...register("priority")}>
+                                    <SelectTrigger>
+                                        <SelectValue  placeholder="Choose Priority" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="{HIGH}">High</SelectItem>
+                                        <SelectItem value="{MEDIUM}">Medium</SelectItem>
+                                        <SelectItem value="{LOW}">Low</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </Field>
+                            <Field>
+                                <FieldLabel htmlFor="home">Home</FieldLabel>
+                                <Select {...register("home")}>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Choose Home" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {
+                                            homeList.map(homeName=> (
+                                                <SelectItem value={homeName.id}>{homeName.name}</SelectItem>
+                                            ))
+                                        }
+                                    </SelectContent>
+                                </Select>
+                            </Field>
+                            <Field>
+                                <FieldLabel htmlFor="status">Status</FieldLabel>
+                                <Controller control={control}
+                                            name="status"
+                                            render={({field}) => (
+                                    <Select onValueChange={field.onChange} value={field.value}>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Choose Status" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value={"OPEN"}>Open</SelectItem>
+                                            <SelectItem value={"IN_PROGRESS"}>In Progress</SelectItem>
+                                            <SelectItem value={"DONE"}>Done</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                )}/>
+                            </Field>
+                            <Field>
+                                <FieldLabel htmlFor="dueDate">Due Date</FieldLabel>
+                                <Input {...register("dueDate",
+                                            {required: true})
+                                        } type="date" autoComplete="off" placeholder={dueDate}/>
+                            </Field>
+                        <Button>
+                            Save
+                        </Button>
+                    </FieldSet>
+                </form>
                 <DialogFooter>
+                    <Button onClick={() => deleteTask(taskSeriesId)}
+                            variant={"destructive"}>
+                        Delete
+                    </Button>
                     <DialogClose asChild>
                         <Button variant="outline">Cancel</Button>
                     </DialogClose>
