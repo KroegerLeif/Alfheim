197d0624e05d0e53e93502f7597de5be6c39b7f0
Leif <Leif.kr@outlook.com>
2025-11-03 13:11:56 +0100

    Item Info will appear by clicking on an Item. Makes more space for detail information and settings button

---
+++ frontend/src/pages/item/ItemInfo.tsx
@@ -0,0 +1,36 @@
+import type {ItemTableReturn} from "@/dto/response/ItemTableReturn.ts";
+import {DialogClose, DialogDescription, DialogFooter, DialogHeader, DialogTitle} from "@/components/ui/dialog.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import ItemSettings from "@/pages/item/ItemSettings.tsx";
+
+
+function ItemInfo(props: Readonly<ItemTableReturn & { loadItemData: () => void }>){
+    const {name, energyLabel, category, homeId} = props;
+    const {loadItemData} = props;
+
+    return(
+        <>
+            <DialogHeader>
+                <DialogTitle className="flex items-center gap-2">
+                    {name}
+                </DialogTitle>
+                <DialogDescription>
+                    Manage item details
+                </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4">
+                EnergyLabel: {energyLabel} <br/>
+                Category: {category} <br/>
+                Home: {homeId}
+            </div>
+            <DialogFooter>
+                <ItemSettings {...props} loadItemData={loadItemData}/>
+                <DialogClose asChild>
+                    <Button variant="outline">Close</Button>
+                </DialogClose>
+            </DialogFooter>
+        </>
+    )
+
+}
+export default ItemInfo;
