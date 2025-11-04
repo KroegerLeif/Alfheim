11f9b40f68cad6c564ba7de8569aadac800879e4
Leif <Leif.kr@outlook.com>
2025-11-04 13:36:59 +0100

    Added Protected Route so only if you are allowed to you can see specific sites

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,17 +6,40 @@ import TaskPage from "@/pages/TaskPage.tsx";
 import ItemPage from "@/pages/ItemPage.tsx";
 import SchedulePage from "@/pages/SchedulePage.tsx";
 import LoginPage from "@/pages/LoginPage.tsx";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import ProtectedRoute from "@/components/ProtectedRoute.tsx";
 
 function App() {
+
+    const [user,setUser] = useState<string | undefined | null>(undefined);
+
+    const loadUser = () => {
+        axios.get("/api/auth")
+            .then((response) => {
+                setUser(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+                setUser(null);
+            }
+            )
+    }
+
+    useEffect(() => {
+        loadUser()
+    }, []);
+
     return (
         <Routes>
-            {/* Alle Routen innerhalb dieses Blocks bekommen die Sidebar */}
-            <Route element={<MainLayout/>} >
-                <Route path="/" element={<MainPage/>} />
-                <Route path="/home" element={<HomePage />} />
-                <Route path={"/task"} element={<TaskPage/>} />
-                <Route path={"/item"} element={<ItemPage/>} />
-                <Route path={"/schedule"} element={<SchedulePage/>} />
+            <Route element={<ProtectedRoute user={user}/>}>
+                <Route element={<MainLayout/>} >
+                    <Route path="/" element={<MainPage/>} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path={"/task"} element={<TaskPage/>} />
+                    <Route path={"/item"} element={<ItemPage/>} />
+                    <Route path={"/schedule"} element={<SchedulePage/>} />
+                </Route>
             </Route>
 
             <Route path="/login" element={<LoginPage/>} />
