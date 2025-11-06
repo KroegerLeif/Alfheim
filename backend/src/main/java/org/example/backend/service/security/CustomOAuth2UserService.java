e7004bfeba27d0017d8191c9ecfde628da7f4d9b
Leif <Leif.kr@outlook.com>
2025-11-06 17:15:25 +0100

    User will now be saved in the DB

---
+++ backend/src/main/java/org/example/backend/service/security/CustomOAuth2UserService.java
@@ -0,0 +1,34 @@
+package org.example.backend.service.security;
+
+import lombok.RequiredArgsConstructor;
+import org.example.backend.domain.user.User;
+import org.example.backend.repro.UserRepro;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepro userRepo;
+
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) {
+
+        OAuth2User oauthUser = super.loadUser(userRequest);
+        User appUser = userRepo.findById(oauthUser.getName())
+                .orElseGet(() -> createAndSaveUser(oauthUser));
+
+        return oauthUser;
+    }
+
+    private User createAndSaveUser(OAuth2User oauthUser) {
+        User newUser = User.builder()
+                .id(oauthUser.getName())
+                .name(oauthUser.getAttribute("login"))
+                .build();
+
+        return userRepo.save(newUser);
+    }
+}
