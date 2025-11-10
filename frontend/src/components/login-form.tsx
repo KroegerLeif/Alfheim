0b171f01ff65ed1aa8b0bca351f49d370f38b1ac
Leif <Leif.kr@outlook.com>
2025-11-10 10:49:30 +0100

    Login From will now only feature O Auth logins...Other Login will follow...Paskey and email login and if possible other

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
 import {
     Card,
     CardContent,
@@ -11,13 +10,9 @@ import {
     Field,
     FieldDescription,
     FieldGroup,
-    FieldLabel,
-    FieldSeparator,
 } from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
 import LoginWithGithub from "@/components/login/LoginWithGithub.tsx";
 import LoginWithGoogle from "@/components/login/LoginWithGoogle.tsx";
-import LoginWithPasskey from "@/components/login/LoginWithPasskey.tsx";
 export function LoginForm({
                               className,
                               ...props
@@ -28,7 +23,7 @@ export function LoginForm({
                 <CardHeader className="text-center">
                     <CardTitle className="text-xl">Welcome back</CardTitle>
                     <CardDescription>
-                        Login with your Google/ Github or create a Passkey
+                        Login with your Google/ Github
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
@@ -37,37 +32,6 @@ export function LoginForm({
                             <Field>
                                 <LoginWithGoogle/>
                                 <LoginWithGithub/>
-                                <LoginWithPasskey/>
-                            </Field>
-                            <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
-                                Or continue with
-                            </FieldSeparator>
-                            <Field>
-                                <FieldLabel htmlFor="email">Email</FieldLabel>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    placeholder="m@example.com"
-                                    required
-                                />
-                            </Field>
-                            <Field>
-                                <div className="flex items-center">
-                                    <FieldLabel htmlFor="password">Password</FieldLabel>
-                                    <a
-                                        href="#"
-                                        className="ml-auto text-sm underline-offset-4 hover:underline"
-                                    >
-                                        Forgot your password?
-                                    </a>
-                                </div>
-                                <Input id="password" type="password" required />
-                            </Field>
-                            <Field>
-                                <Button type="submit">Login</Button>
-                                <FieldDescription className="text-center">
-                                    Don&apos;t have an account? <a href="#">Sign up</a>
-                                </FieldDescription>
                             </Field>
                         </FieldGroup>
                     </form>
